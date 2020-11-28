a0f5b8d2149bb0b04c076d1451f23af609266a5e
Marek Anderson <marek.anderson@arctiq.ca>
2020-11-28 12:31:50 -0500

    reduce vars file

--- variables.tf
+++ variables.tf
@@ -2,15 +2,3 @@ variable "lp" {
   description = "Name of licensePlate"
   type        = string
 }
-
-/*
-variable "lp_nonprod" {
-  description = "licensePlate plus -nonprod suffix"
-  type        = string
-}
-
-variable "lp_prod" {
-  description = "licensePlate plus -prod suffix"
-  type        = string
-}
-*/
